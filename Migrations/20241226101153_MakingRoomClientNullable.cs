4040f0028e4f513b1882d118abebde11518e0174
lorenzoarci <Arcidiacono.lorenzo2@gmail.com>
2024-12-26 11:13:02 +0100

    Making room and client nullable

---
+++ Migrations/20241226101153_MakingRoomClientNullable.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakingRoomClientNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Clients_ClientId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Bookings",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Bookings",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Clients_ClientId",
+                table: "Bookings",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Clients_ClientId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Bookings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Bookings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Clients_ClientId",
+                table: "Bookings",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "RoomId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
